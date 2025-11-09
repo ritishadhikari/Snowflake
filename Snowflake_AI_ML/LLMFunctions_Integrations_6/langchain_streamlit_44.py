fa540c3fc61b7d4bbb123e4ef94e3d7b3299537e
Ritish Adhikari <ritishadhikari@gmail.com>
2025-11-09 19:21:39 +0530

    Till 11/09/2025 19:21:39

---
+++ Snowflake_AI_ML/LLMFunctions_Integrations_6/langchain_streamlit_44.py
@@ -0,0 +1,36 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+import os
+from langchain_community.utilities import SQLDatabase
+from langchain_openai import OpenAI
+from langchain.chains import create_sql_query_chain
+
+
+# Get the current credentials
+
+
+@st.cache_resource(show_spinner="Connecting ...")
+def getSession():
+    session = get_active_session()
+    url="snowflake://ritishadhikari:Eklavya@05072024@DLCTVSW-DW86981/test/public?warehouse=compute_wh&role=accountadmin"
+    db=SQLDatabase.from_uri(url)
+    openai_key='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'
+    llm=OpenAI(openai_api_key=openai_key)
+    chain=create_sql_query_chain(llm, db)
+    return session, db, chain
+
+st.title("SQL Query Generator")
+st.write("Returns and runs queries from questions in natural language")
+
+session, db, chain=getSession()
+
+question=st.sidebar.text_area("Ask a question:", value="Show me the total number of entries in the first table")
+
+sql=chain.invoke({"question":question}).rstrip(';')
+
+tabQuery,tabData, tabLog=st.tabs(['Query',"Data",'Log'])
+
+tabQuery.code(sql,language='sql')
+tabdata.dataframe(session.sql(sql))
+tabLog.code(db.table_info, language='sql')
